13c48abc8343e69d64a2cb23cd938b22a540137d
hannes <hannes.study@gmail.com>
2024-07-16 10:17:31 +0300

    maths v1

---
+++ maths.js
@@ -0,0 +1,19 @@
+const a = 1;
+const b = 3;
+let result = undefined;
+
+function add(a, b) {
+	result = a + b;
+	return result;
+}
+
+function subtract(a, b) {
+	result = a - b;
+	return result;
+}
+
+function multiply(a, b) {
+	result = a * b;
+	return result;
+}
+maths;
