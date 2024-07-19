e176d88b79370d61c94669d6ca4ea550a994ea52
hannes <hannes.study@gmail.com>
2024-07-19 11:40:27 +0300

    add-flat v3

--- add-flat.js
+++ add-flat.js
@@ -1,28 +1,22 @@
 function sumNestedArray(arr) {
 	let flatArr;
 	let sum;
+
 	if (Array.isArray(arr)) {
 		flatArr = arr.flat(Infinity);
 		sum = sumArray(flatArr);
 	}
+    
 	return undefined;
 }
 
 function sumArray(arr) {
-	let sum = null;
-	if (Array.isArray(arr)) {
-		let arrLength = arr.length;
-		for (let i = 0; i < arrLength; i++) {
-			if (typeof arr[i] === "number") {
-				if (sum === null) {
-					sum = 0;
-				}
-				sum += arr[i];
-			}
-		}
-		if (sum !== null) {
-			return sum;
+	let sum = 0;
+	let arrLength = arr.length;
+	for (let i = 0; i < arrLength; i++) {
+		if (typeof arr[i] === "number") {
+			sum += arr[i];
 		}
 	}
-	return undefined;
+	return sum;
 }
