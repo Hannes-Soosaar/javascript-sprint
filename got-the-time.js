b027f9277ea989d2c16d59427779f75d45c75559
hannes <hannes.study@gmail.com>
2024-07-24 10:07:48 +0300

    got-the-time.js

---
+++ got-the-time.js
@@ -0,0 +1,66 @@
+function printPrettyDate(date) {
+	let year = date.getFullYear();
+	let month = date.getMonth();
+	let day = date.getDay();
+	let dayDate = date.getDate();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let second = date.getSeconds();
+	let amPm = getAmPM(hours);
+
+	if (amPm === "PM") {
+		hours = getAmPmTimeFormat(hours);
+	}
+
+	const daysOfWeek = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const monthsOfYear = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	return (
+		"Today is " +
+		daysOfWeek[day] +
+		", " +
+		monthsOfYear[month] +
+		" " +
+		dayDate +
+		", " +
+		year +
+		", and the time is " +
+		hours +
+		":" +
+		minutes +
+		":" +
+		second +
+		" " +
+		amPm +
+		"."
+	);
+}
+
+function getAmPM(hours) {
+	return hours >= 12 ? "PM" : "AM";
+}
+
+function getAmPmTimeFormat(hours) {
+	return hours % 12;
+}
