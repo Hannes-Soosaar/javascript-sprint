31781300baaaf7f6cf90866d61956d6584009e26
hannes <hannes.study@gmail.com>
2024-07-24 19:04:16 +0300

    array-filters test 2

--- array-filter.js
+++ array-filter.js
@@ -1,26 +1,22 @@
-
 function isEven(number) {
 	return number % 2 === 0;
 }
 
-function filterOutOddNumbers(arr){
-   return arr.filter(isEven);
+function filterOutOddNumbers(arr) {
+	return arr.filter(isEven);
 }
 
-function filterObjectsByNameLength(arr,length){
-    return arr.filter(object => nameIsLongerThan(object,length))
+function filterObjectsByNameLength(objects, length) {
+	return objects.filter((object) => nameIsLongerThan(object, length));
 }
 
 function nameIsLongerThan(object, number) {
-	return object.name.length > number;
+	return object.name.length >= number;
 }
 
-function compoundFilter(arr){
-
-    // no "code" greater then 5 characters
-    // "category" does not contain `special`
-    // "price" is greater than 50 
-    // "location" is not equal to `Underground`
+function compoundFilter(arr) {
+	// no "code" greater then 5 characters
+	// "category" does not contain `special`
+	// "price" is greater than 50
+	// "location" is not equal to `Underground`
 }
-
-
