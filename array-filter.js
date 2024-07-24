8aa0460239d57307091bccac94c7dddec574c230
hannes <hannes.study@gmail.com>
2024-07-24 10:49:00 +0300

    array-filter js test1

---
+++ array-filter.js
@@ -0,0 +1,32 @@
+
+function isEven(number) {
+	return number % 2 === 0;
+}
+
+function filterOutOddNumbers(arr){
+   return arr.filter(isEven);
+}
+
+const objects = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+	{ id: 3, name: "Charlie" },
+];
+
+function filterObjectsByNameLength(arr,length){
+    return arr.filter(object => nameIsLongerThan(object,length))
+}
+
+function nameIsLongerThan(object, number) {
+	return object.name.length > number;
+}
+
+function compoundFilter(arr){
+
+    // no "code" greater then 5 characters
+    // "category" does not contain `special`
+    // "price" is greater than 50 
+    // "location" is not equal to `Underground`
+}
+
+
