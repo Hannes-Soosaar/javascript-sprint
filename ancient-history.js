722bd6026ef9a4823555f542a4a5cbf1bf58387a
hannes <hannes.study@gmail.com>
2024-07-24 09:33:02 +0300

    ancient-history.js

--- ancient-history.js
+++ ancient-history.js
@@ -1,21 +1,46 @@
-function classifyDate(date){
+function classifyDate(date) {
+	if (!(date instanceof Date)) {
+		return "invalid date object";
+	}
 
-if (!(date instanceof Date)){
-    return "invalid date object"
+    let deltaYear = getYearDifference(date)
+
+	if ( deltaYear < -1) {
+		return "ancient";
+	} else if (deltaYear > 1) {
+		return "distant future";
+	} else if (timeNow < date) {
+		return "future";
+	} else if (timeNow > date) {
+		return "past";
+	} else {
+		return "present";
+	}
 }
 
-    let timeNow = new Date();
-    let yearNow = timeNow.getFullYear()
-    let yearCompare = date.getFullYear()
-    if ( yearNow > yearCompare + 1) {
-        return "ancient";
-    } else if (yearNow < yearCompare-1) {
-        return "distant future";
-    } else if ( timeNow < date){
-        return "future";
-    } else if ( timeNow > date){
-        return "past";
-    } else {
-        return "present";
-    }
+function getYearDifference(date2) {
+	let date1 = new Date();
+	let date2IsInThePast = false;
+
+	if (date1 > date2) {
+		[date1, date2] = [date2, date1];
+		date2IsInThePast = true;
+	}
+
+	let year1 = date1.getFullYear();
+	let year2 = date2.getFullYear();
+	let month1 = date1.getMonth();
+	let month2 = date2.getMonth();
+	let day1 = date1.getDate();
+	let day2 = date2.getDate();
+	let deltaYear = year2 - year1;
+
+	if (month2 < month1 || (month2 === month1 && day2 < day1)) {
+		deltaYear--;
+	}
+	if (date2IsInThePast) {
+		deltaYear  = -deltaYear;
+	}
+
+	return deltaYear;
 }
