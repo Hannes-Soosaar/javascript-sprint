721138d3ed5eae7952985aefc6744f7c79b81e4a
hannes <hannes.study@gmail.com>
2024-07-25 10:56:38 +0300

    diesel v1

---
+++ diesel.js
@@ -0,0 +1,36 @@
+
+
+const carData =
+	'[["Toyota", "Camry", "ABC123", 2014, "diesel"], ["Ford", "Focus", "XYZ456", 1999, "petrol"]]';
+
+function calculateFines(carData) {
+	const cars = JSON.parse(carData);
+	let totalFines = 0;
+    const fineForOldVehicles = 20;
+	const fineForDieselVehicles = 10;
+	const finedCars = [];
+	cars.forEach((car) => {
+		const [make, model, reg, year, fuel] = car;
+		let fine = 0;
+		if (year < 2000) {
+			fine = Math.max(fine, fineForOldVehicles);
+		}
+		if (fuel === `diesel` && year < 2015) {
+			fine = Math.max(fine, fineForDieselVehicles);
+		}
+		if (fine > 0) {
+			totalFines += fine;
+			finedCars.push({
+				reg: reg,
+				year: year,
+				fuel: fuel,
+				fine: fine,
+			});
+		}
+	});
+	const result = {
+		totalFines: totalFines,
+		cars: finedCars,
+	};
+    return JSON.stringify(result)
+}
